9c50172af7b4575ad1563a90021e2c6bda4f8bef
Dogeishon <75345913@continental.edu.pe>
2025-10-23 11:08:44 -0500

    Mock BStrp

--- src/frontend/tests/setup/jest.setup.extra.js
+++ src/frontend/tests/setup/jest.setup.extra.js
@@ -5,3 +5,18 @@ import { restoreFetch } from "./fetch.mock";
 afterEach(() => {
   restoreFetch();
 });
+
+jest.mock('react-bootstrap', () => {
+  const React = require('react');
+  const make = (name) => React.forwardRef((props, ref) =>
+    React.createElement('div', { 'data-mock': name, ref, ...props }, props.children)
+  );
+  return {
+    __esModule: true,
+    Navbar: make('Navbar'),
+    Nav: make('Nav'),
+    Container: make('Container'),
+    Button: make('Button'),
+    NavDropdown: make('NavDropdown'),
+  };
+});
